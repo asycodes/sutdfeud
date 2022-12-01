99428bce518aa75ee36c0b085949efa6bc8bab96
Asyraf Omar <asyrafomar68@gmail.com>
2022-12-01 21:48:16 +0800

    idk lah

--- familyfeud.py
+++ familyfeud.py
@@ -1,31 +1,71 @@
 from tkinter import *
-from time import sleep
 
 
-
-#SinglePlayer Frame
-def single():
-    hide_frames()
-    single_frame.pack(fill='both',expand=1)
-    heading = Label(single_frame,text ='Alone').pack()
-
-#Multiplayer Frame
-def multi():
-    hide_frames()
-    multi_frame.pack(fill='both',expand=1)
-    heading = Label(multi_frame, text ='w Frends').pack()
+#file1 = open('feuddata/singlenames.txt')
 
 
-# HOme Frame
+# ----------HOme Frame----------
 def home():
     hide_frames()
     start_frame.pack(fill='both',expand =1 )
 
-# hide prev frames
+#----------SinglePlayer Frame----------
+def single():
+    hide_frames()
+    single_frame.pack(fill='both',expand=1)
+    heading = Label(single_frame,text ='Alone').pack()
+    textbox = Text(single_frame,width=40,height=10,background= "gray71",foreground="#fff",font= ('Sans Serif', 13, 'italic bold'))
+    textbox.insert(INSERT,"Your name")
+    textbox.pack()
+    btn3 = Button(single_frame,text='Enter',command=singlematch(textbox.get('1.0',END))) #get their name
+    btn3.pack() # proceed to the game
+
+def singlematch(name='none'):
+    hide_frames()
+    mylabel = Label(singlematch_frame,text='').pack()
+    singlematch_frame.pack(fill='both',expand=1)
+    mylabel.config
+    
+    
+
+#---------Multiplayer Frame----------
+def multi():
+    hide_frames()
+    multi_frame.pack(fill='both',expand=1)
+    heading = Label(multi_frame, text ='w Frends').pack()
+    textbox1 = Text(multi_frame,width=40,height=10,font= ('Sans Serif', 13, 'italic bold'))
+    textbox2 = Text(multi_frame,width=40,height=10,font= ('Sans Serif', 13, 'italic bold'))
+    textbox1.insert(INSERT,"Name for Group 1")
+    textbox2.insert(INSERT,"Name for Group 2")
+    textbox1.pack()
+    textbox2.pack()
+    btn3 = Button(multi_frame,text='Enter',command=get_text) #get their name
+    btn3.pack() # proceed to the game
+
+def multimatch():
+    pass 
+
+# ----------hide prev frames----------
+'''everytime u add a new frame/page, u have to update here'''
 def hide_frames():
+    # to hide multiple iterations of the same frames
+    for widget in multi_frame.winfo_children():
+        widget.destroy()
+    for widget in single_frame.winfo_children():
+        widget.destroy()
+    # to hide previous frames 
     multi_frame.pack_forget()
     single_frame.pack_forget()
     start_frame.pack_forget()
+    singlematch_frame.pack_forget()
+
+def get_text():
+    pass
+
+
+
+
+#---------underneath here is the initialization-----------
 
 root = Tk()
 root.title('SUTD Fued')
@@ -40,13 +80,14 @@ mymenu.add_command(label = 'home', command = home)
 
 #FRAMES (something like divs in html)
 start_frame = Frame(root, width=500,height=500) #home page
-start_frame.pack(fill='both',expand=1)
 multi_frame = Frame(root, width=500,height=500) #page for multiplayer
 single_frame = Frame(root, width=500,height=500) #page for singleplayer
+singlematch_frame = Frame(root, width=500,height=500)
+start_frame.pack(fill='both',expand=1)
 
 
 
-
+#header
 heading_text = Label(start_frame,text = 'Welcome to SUTD Feud',font=('Helvetica',25)).pack(pady=20)
 # Singlepayer button
 btn1 = Button(start_frame,command= single , text='alone lol').pack()
@@ -54,65 +95,5 @@ btn1 = Button(start_frame,command= single , text='alone lol').pack()
 btn2 =Button(start_frame, command = multi, text='u have friends').pack()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-
-
-used to select game mode : 
-singleplayer - j by urself lorh.
-
-Multiplayer - against 2 players or 2 group of players
-
-
-def modepage():
-    global modep
-    modep = Toplevel()
-    modep.title('Game Mode')
-    modep.geometry("800x500")
-    btn2 = Button(modep,text ='Single Player', command =gamepage1).pack()
-    btn3 = Button(modep,text ='Multiplayer', command =gamepage2).pack()
-
-
-
-
-def gamepage1():
-    gpage1 = Toplevel()
-    gpage1.title('SUTD Feud Alone LOL')
-    gpage1.geometry("800x500")
-    modep.destroy()
-    btn4 = Button(modep,text ='Go back', command =modepage).pack()
-
-    gamepage1.destroy
-
-def gamepage2():
-    gpage2 = Toplevel()
-    gpage2.title('SUTD Feud Alone LOL')
-    gpage2.geometry("800x500")
-    modep.destroy()
-
-def endwindow():
-    pass
-
-'''
 
 mainloop()
